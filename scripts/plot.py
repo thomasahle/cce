e7d062a92db635f6ecf767d174322f27ec138b72
Thomas D Ahle <thomas@ahle.dk>
2023-08-07 17:56:06 +0200

    Added 1m results

--- scripts/plot.py
+++ scripts/plot.py
@@ -2,21 +2,26 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
+main_title = None
 data = {}
 with open(sys.argv[1]) as f:
     for line in f:
+        if line.startswith('# '):
+            main_title = line.strip('#').strip()
         if line.startswith('##'):
             method = line.strip('#').strip()
             data[method] = []
         elif line[0].isdigit():
             ppd, *seeds = map(float, line.split())
             # ppd > 1900 doesn't make sense, as it's bigger than the vocab
-            if ppd > 1900:
-                continue
+            #if ppd > 1900:
+                #continue
             data[method].append((ppd, seeds))
 
 plt.figure()
 for method, values in data.items():
+    if method == 'robe':
+        continue
     x, y = zip(*values)
     y_median = [np.median(triple) for triple in y]
     y_lower = [min(triple) for triple in y]
@@ -29,5 +34,5 @@ plt.legend()
 plt.xlabel('Params/dim')
 plt.xscale('log')
 plt.ylabel('BCE')
-plt.title('ml100k best of 10 epochs')
+plt.title(main_title)
 plt.show()
